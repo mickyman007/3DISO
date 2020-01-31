792d35cde45989e3118cbc345760cf1a2db7027d
Michael Forrester <mickyman00777@gmail.com>
2020-01-31 22:33:00 +0000

    Added reading spaces from array & moving prefabs to resources folder

--- Assets/scripts/Board/BoardSetter.cs
+++ Assets/scripts/Board/BoardSetter.cs
@@ -1,11 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class BoardSetter : MonoBehaviour {
+
+    public char[,] myBoard = {
+        { 'W', 'W', 'W', 'W', 'W'},
+        { 'W', 'S', 'S', 'S', 'W'},
+        { 'W', 'S', 'S', 'S', 'W'},
+        { 'W', 'S', 'S', 'S', 'W'},
+        { 'W', 'W', 'W', 'W', 'W'}
+    };
 
     public int GridSizeX;
     public int GridSizeY;
     public IBoard Board;
     public GameObject EmptySpace;
+    public GameObject Space;
+    public GameObject Wall;
+
+    public Dictionary<char, GameObject> spaceDictionary;
 
     private void SetupBoard() {
         Board = new Board(GridSizeX, GridSizeY);
@@ -19,16 +32,24 @@ public class BoardSetter : MonoBehaviour {
     }
 
     private GameObject CreateSpace(int x, int y) {
-        var spaceGo = Instantiate(EmptySpace, BoardUtilities.CoordToWorld(x, y), Quaternion.Euler(90, 0, 0));
+        var space = myBoard[x, y];
+        var spaceGo = Instantiate(spaceDictionary[myBoard[x,y]], BoardUtilities.CoordToWorld(x, y), Quaternion.Euler(90, 0, 0));
         spaceGo.AddComponent<Space>().Initialise(x, y);
         return spaceGo;
     }
 
     void Start() {
-        SetupBoard();
-    }
+        GridSizeX = myBoard.GetLength(0);
+        GridSizeY = myBoard.GetLength(1);
 
-    void Update() {
-        
+        Space = Resources.Load("prefabs/Space") as GameObject;
+        Wall = Resources.Load("prefabs/WallSpace") as GameObject;
+
+        spaceDictionary = new Dictionary<char, GameObject> {
+            { 'S', Space},
+            { 'W', Wall}
+        };
+
+        SetupBoard();
     }
 }
