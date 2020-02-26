bbe3886e23f063d743dd5cf341ee7cbd8399a398
Michael Forrester <mickyman00777@gmail.com>
2020-02-26 20:52:04 +0000

    Updating SpaceWall to inherit most functionality from the base Space

--- Assets/scripts/Board/Spaces/SpaceWall.cs
+++ Assets/scripts/Board/Spaces/SpaceWall.cs
@@ -1,58 +1,12 @@
 ﻿using System;
 using UnityEngine;
 
-public class SpaceWall : MonoBehaviour, ISpace {
+public class SpaceWall : Space {
     private bool isSelected;
 
-    public int X { get; private set; }
-
-    public int Y { get; private set; }
-
-    public void Initialise(int x, int y) {
-        this.X = x;
-        this.Y = y;
-        transform.name = typeof(Space).ToString() + "(" + x + ", " + y + ")";
+    public new void Initialise(int x, int y) {
+        base.Initialise(x, y);
         CanSelect = false;
         CanMove = false;
-    }
-
-    public bool IsSelected {
-        get { return isSelected; }
-        set { 
-            if(isSelected == value) {
-                return;
-            }
-            isSelected = value;
-            if (value) {
-                Select();
-            } else {
-                Unselect();
-            }
-        }
-    }
-
-    public bool CanSelect { get; set; }
-
-    public bool CanMove { get; set; }
-
-    public event EventHandler OnSelection;
-
-    private void Select() {
-        transform.localScale *= 1.2f;
-        Debug.Log("Selected" + transform.name);
-        OnSelection(this, new EventArgs());
-    }
-
-    private void Unselect() {
-        transform.localScale /= 1.2f;
-        Debug.Log("Unselected" + transform.name);
-    }
-
-    void OnDrawGizmos() {
-        Gizmos.color = isSelected ? Color.green : Color.red;
-
-        Gizmos.DrawWireCube(
-            new Vector3(X, transform.position.y + 0.5f, Y),
-            new Vector3(0.5f, 1, 0.5f));
     }
 }
