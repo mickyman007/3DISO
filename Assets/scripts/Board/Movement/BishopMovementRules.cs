338cd48d3a654e05e25b5f32ee39deafbceaac28
Michael Forrester <mickyman00777@gmail.com>
2020-06-16 23:30:26 +0100

    Bishop can no longer move through other peices.

--- Assets/scripts/Board/Movement/BishopMovementRules.cs
+++ Assets/scripts/Board/Movement/BishopMovementRules.cs
@@ -6,16 +6,91 @@ public class BishopMovementRules : IMovementRules {
         var pieceSpace = piece.SpaceOccupied;
         var spaces = new List<ISpace>();
 
-        // TODO try not to loop through EVERY space on the board
-        foreach (var boardSpace in board.Spaces) {
-            var x = boardSpace.X - pieceSpace.X;
-            var y = boardSpace.Y - pieceSpace.Y;
-
-            if (Math.Abs(x) == Math.Abs(y)) {
-                spaces.Add(boardSpace);
-            }
-        }
+        spaces.AddRange(getNorthEastSpaces(board, pieceSpace));
+        spaces.AddRange(getNorthWestSpaces(board, pieceSpace));
+        spaces.AddRange(getSouthEastSpaces(board, pieceSpace));
+        spaces.AddRange(getSouthWestSpaces(board, pieceSpace));
 
         return spaces.ToArray();
+    }
+
+    private IEnumerable<ISpace> getNorthWestSpaces(IBoard board, ISpace space) {
+        var spaces = new List<ISpace>();
+
+        var x = space.X - 1;
+        var y = space.Y + 1;
+
+        while (board.IsInBounds(x, y)) {
+            if (board.Pieces[x, y] == null) {
+                spaces.Add(board.Spaces[x, y]);
+            } else {
+                break;
+            }
+
+            x--;
+            y++;
+        }
+
+        return spaces;
+    }
+
+    private IEnumerable<ISpace> getNorthEastSpaces(IBoard board, ISpace space) {
+        var spaces = new List<ISpace>();
+
+        var x = space.X + 1;
+        var y = space.Y + 1;
+
+        while (board.IsInBounds(x, y)) {
+            if (board.Pieces[x, y] == null) {
+                spaces.Add(board.Spaces[x, y]);
+            } else {
+                break;
+            }
+
+            x++;
+            y++;
+        }
+
+        return spaces;
+    }
+
+    private IEnumerable<ISpace> getSouthWestSpaces(IBoard board, ISpace space) {
+        var spaces = new List<ISpace>();
+
+        var x = space.X - 1;
+        var y = space.Y - 1;
+
+        while (board.IsInBounds(x, y)) {
+            if (board.Pieces[x, y] == null) {
+                spaces.Add(board.Spaces[x, y]);
+            } else {
+                break;
+            }
+
+            x--;
+            y--;
+        }
+
+        return spaces;
+    }
+
+    private IEnumerable<ISpace> getSouthEastSpaces(IBoard board, ISpace space) {
+        var spaces = new List<ISpace>();
+
+        var x = space.X + 1;
+        var y = space.Y - 1;
+
+        while (board.IsInBounds(x, y)) {
+            if (board.Pieces[x, y] == null) {
+                spaces.Add(board.Spaces[x, y]);
+            } else {
+                break;
+            }
+
+            x++;
+            y--;
+        }
+
+        return spaces;
     }
 }
